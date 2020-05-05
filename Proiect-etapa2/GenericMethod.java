67426f31a8b1a82844b2400c9c7ac2323f1cd8ab
IonitaAdriana <61555060+IonitaAdriana@users.noreply.github.com>
2020-05-05 23:02:01 +0300

    Add files via upload

---
+++ Proiect-etapa2/GenericMethod.java
@@ -0,0 +1,54 @@
+import datePersoane.Angajati;
+import datePersoane.Client;
+import Produse.Carte;
+import Produse.Boardgame;
+
+
+import java.util.ArrayList;
+
+public class GenericMethod {
+
+        public static ArrayList<Angajati> void printGenericAngajati(ArrayList<Angajati> inputArray ) {
+
+            for(Angajati element : inputArray) {
+                System.out.printf(element.getNume() +" "+ element.getDepartament()+" "+ element.getSalariu());
+            }
+            System.out.println();
+        }
+
+    public static ArrayList<Client> void printGenericAngajati(ArrayList<Client> inputArray ) {
+
+        for(Client element : inputArray) {
+            System.out.printf(element.getNume() +" " + element.getVarsta()+ " " +element.getNrPuncte());
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<Client> void printGenericClienti(ArrayList<Client> inputArray ) {
+
+        for(Client element : inputArray) {
+            System.out.printf(element.getNume() +" " + element.getVarsta()+ " " +element.getNrPuncte());
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<Carte> void printGenericCarte(ArrayList<Carte> inputArray ) {
+
+        for(Carte element : inputArray) {
+            System.out.printf(element.getTitlu() +" " + element.getAutor());
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<Boardgame> void printGenericBoardgames(ArrayList<Boardgame> inputArray ) {
+
+        for(Boardgame element : inputArray) {
+            System.out.printf(element.getTip() +" " + element.getDurata()+ " " +element.getVarsta());
+        }
+        System.out.println();
+    }
+
+
+
+
+}
